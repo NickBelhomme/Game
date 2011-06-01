3c2ed65686117325c947259dc8765de9e212c2e0
NickBelhomme <contact@nickbelhomme.com>
2011-06-01 17:26:02 +0200

    changed the placement of the form

--- Application/Library/Response.php
+++ Application/Library/Response.php
@@ -3,6 +3,6 @@ namespace App;
 class Response extends \Game\Response
 {
     protected $messages = array(
-        10000 => '<form method="get">    <input type="text" name="cmd" id="commandPrompt" />    </form>    <script type="text/javascript">       function formfocus() {          document.getElementById(\'commandPrompt\').focus();       }       window.onload = formfocus;    </script>'
+        'x' => '<form method="get">    <input type="text" name="cmd" id="commandPrompt" />    </form>    <script type="text/javascript">       function formfocus() {          document.getElementById(\'commandPrompt\').focus();       }       window.onload = formfocus;    </script>'
     );
 }
