dc1801b0ed7671fa2419e2e83fc8883f544cbac6
NickBelhomme <contact@nickbelhomme.com>
2011-06-01 17:39:04 +0200

    added annotations to Game\Response

--- Library/Game/Response.php
+++ Library/Game/Response.php
@@ -2,14 +2,36 @@
 namespace Game;
 class Response
 {
+    /**
+     * the actual response return value
+     * @var array
+     */
     protected $messages = array();
+
+    /**
+     * The response will be returned as a string
+     * This separator will implode the messages together
+     * @var string
+     */
     protected $separator = PHP_EOL;
 
+    /**
+     * add a message to the response
+     *
+     * @param string $message
+     * @return Game\Response
+     */
     public function addMessage($message)
     {
         $this->messages[] = $message;
+        return $this;
     }
 
+    /**
+     * return the response as a string
+     *
+     * @return string
+     */
     public function output()
     {
         ksort($this->messages);
